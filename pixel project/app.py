c9bb57263750ea0b71760163919cea4ee999e3c6
Itamar9494 <itamar9494@gmail.com>
2023-06-03 23:44:06 +0300

    final changes

--- pixel project/app.py
+++ pixel project/app.py
@@ -1,3 +1,24 @@
 from utils import show_picture
-file_name = 'jack.jpg' #put your file's name here
-show_picture(file_name)
+import os
+
+folder_path = "files"
+
+files = os.listdir(folder_path)
+
+for i, file in enumerate(files):
+    if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg") :
+        print(f"{i+1}. {file}")
+
+choice = int(input("Which image would you like to choose? Enter the corresponding number: "))
+
+choice = 0
+while choice < 1 or choice > len(files):
+    choice = int(input("Which image would you like to choose? Enter the corresponding number: "))
+    if choice >= 1 and choice <= len(files):
+        break
+    print(f'You have to choose a number between 1 and {len(files)}')
+
+chosen_picture = files[choice-1]
+print(f"You chose: {chosen_picture}")
+
+show_picture(chosen_picture)
